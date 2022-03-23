1311db756466a315b7cea5aefa188dd72e2b2854
shenbagad <shenbagadevi94@gmail.com>
2022-03-23 19:02:19 +0530

    Added getuser feature

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.demo.dto.UserRequest;
 import com.example.demo.exception.CustomCreateUserException;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
+import com.example.demo.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +19,20 @@ public class UserController {
 
         @Autowired
         UserService userService;
+
     @GetMapping("/getUsers")
     public ResponseEntity<List<User>> getUsers(){
         return new ResponseEntity<>(userService.getUsers(),HttpStatus.FOUND);
     }
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserRequest userRequest){
+        try {
+            UserServiceImpl userService;
+            return new ResponseEntity<>(userService.createUser(userRequest), HttpStatus.CREATED);
+        }
+        catch (Exception e){
+            throw new CustomCreateUserException("Syntax Error");
+        }
 
 }
 
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -8,4 +8,5 @@ import java.util.List;
 
 public interface UserService {
     public List<User> getUsers();
+    UserDto createUser(UserRequest userRequest);
 }
